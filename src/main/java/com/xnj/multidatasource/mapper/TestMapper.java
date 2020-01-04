2eef95cdebcc218dd3ba8b61c2d0b25dfb74fa04
unknown <xieningjie@wasion.cn>
2020-01-04 11:12:36 +0800

    first commit

---
+++ src/main/java/com/xnj/multidatasource/mapper/TestMapper.java
@@ -0,0 +1,16 @@
+package com.xnj.multidatasource.mapper;
+
+import com.xnj.multidatasource.entity.Test;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author NingkitTse
+ * @since 2020-01-04
+ */
+public interface TestMapper extends BaseMapper<Test> {
+
+}
---
+++ src/main/java/com/xnj/multidatasource/service/ITestService.java
@@ -0,0 +1,16 @@
+package com.xnj.multidatasource.service;
+
+import com.xnj.multidatasource.entity.Test;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author NingkitTse
+ * @since 2020-01-04
+ */
+public interface ITestService extends IService<Test> {
+
+}
